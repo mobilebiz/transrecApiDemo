20789905cea8b1bb7e93893bb8f5e374cd57e37b
Katsumi Takahashi <katsumi@gij.com>
2016-05-21 19:28:21 +0900

    kintone書き出し

--- routes/index.js
+++ routes/index.js
@@ -140,6 +140,7 @@ router.post('/ping', function(req, res, next) {
           callback(null, 'Kintoneに書き出しました。');
         } else {
           debug('error: '+ response.statusCode);
+          debug(body.errors);
           debug(body.errors.app.messages);
           callback(new Error('Kintoneへ書き出し中に、エラーが発生しました。'));
         }
