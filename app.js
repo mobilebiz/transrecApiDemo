b6a4267c641b6aea689f96ce892644fbcab46fcc
Katsumi Takahashi <katsumi@gij.com>
2016-05-21 16:25:20 +0900

    Debug

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/ping', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
